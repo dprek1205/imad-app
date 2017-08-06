543883a1ebe2a1459b333edcbc1ce12873e5f4cd
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-06 22:19:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ var nameinp=document.getElementById("name");
 var name=nameinp.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
-    var names=['name1','name2','name3'];
+    var names=['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<names.length;i++)
     {
