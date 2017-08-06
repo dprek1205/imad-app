2c4c5df5d17fe339883b4044a6c0098d6d0bbf97
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-06 22:04:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,16 @@ button.onclick= function(){
 request.open('GET',"http://deepa042008.imad.hasura-app.io/counter",true);
 request.send(null);
 };
+var nameinp=document.getElementById("name");
+var entered_name=nameinp.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById("namelist");
+    ul.innerHTML=list;
+}
