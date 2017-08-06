4e3a336a7a60737a691ae1e542044e01b7ce325a
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-06 21:33:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,24 @@
 console.log('Loaded!');
 
-//make a request to counter endpoint
-//capture thre sp *& store in var
+
+//capture the resp & store 
 //render the var in span
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick= function(){
-    counter++;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
+    //create a request to web server
+    var  request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+        
+    }; /*end of onready*/
+//make a request to counter endpoint
+request.open('GET',"http://deepa042008.imad.hasura-app.io/counter",true);
+request.send(null);
 };
