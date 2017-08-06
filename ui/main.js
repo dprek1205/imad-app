fb6cf107778de85ae23aeaa62913e650c3953ce0
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-06 22:52:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,12 +26,25 @@ var nameinp=document.getElementById("name");
 var name=nameinp.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById("namelist");
-    ul.innerHTML=list;
+  //  var names=['name1','name2','name3','name4'];
+    var  request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var names=req.responseText;
+                //converts string i.e responsetext to an object 
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++)
+                {
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById("namelist");
+                ul.innerHTML=list;
+                
+            }
+        }
+};
+request.open('GET','http://deepa042008.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
 };
