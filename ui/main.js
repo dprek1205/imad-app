99974a70c893f4fcfda50b180fce8486b70edb88
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-06 19:50:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,14 +5,14 @@ ele.innerHTML='appolo spacecraft';
 var img=document.getElementById('madi');
 var marginLeft=0;
 var K=0;
-function moveRight(){
+/*function moveRight(){
     marginLeft+=1;
     img.style.marginLeft=marginLeft+'px';
-}
+}*/
 
 img.onclick=function(){
-  //  img.style.marginLeft = '100px';
-  var interval=setInterval(moveRight,50);
+    img.style.marginLeft = '100px';
+  //var interval=setInterval(moveRight,50);
 };
 k++1;
     alert('k is '+k);
