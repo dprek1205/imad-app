3bfb1480db044d551f5319ee059f6d9423d86bda
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-06 21:16:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,9 @@
 console.log('Loaded!');
-//var ele=document.getElementById('main-text');
-//ele.innerHTML='appolo spacecraft';
 
 //make a request to counter endpoint
 //capture thre sp *& store in var
 //render the var in span
-var counter=document.getElementById('counter');
+var button=document.getElementById('counter');
 button.onclick= function(){
     counter++;
     var span=document.getElementById('count');
