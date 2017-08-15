a6d186be6498ce58631b9ba221a0f742973de7fd
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-15 12:47:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,8 @@ app.get('/articles/:articleName',function(req,res){
     //make req
     //create a response
 
-    pool.query("select * from article where title='"+req.params.articleName+"'",function(err,result){
+    //pool.query("select * from article where title='"+req.params.articleName+"'",function(err,result){
+    pool.query("select * from article where title=$1",[req.params.articleName],function(err,result){
         if (err){
             res.status(500).send(err.toString());
         }
