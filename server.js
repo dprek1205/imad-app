6e002d91104a4866f1033cd16ad7e73eed898563
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-17 23:25:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -180,6 +180,21 @@ app.post('/login',function(req,res){
     var password=req.body.password;
     pool.query('SELECT * FROM "user" WHERE username = $1',[username],function(err,result){
         //console.log('in pool query');
+        var dbString=result.rows[0].password;
+              var salt=dbString.spilt('$')[2];
+              
+              //hash the user entered password after adding SALT & check this with what was stored in table
+              //console.log('before calling hash fn');
+              
+              var hashedString=hash(password,salt);
+              //console.log('before comparing'+hashedString+":"+dbString);
+              
+              if (hashedString === dbString){
+         
+                 res.send('credentials are corrrect');
+              }
+              
+        /*
         if (err)
         {
             res.status(500).send(err.toString());
@@ -211,6 +226,7 @@ app.post('/login',function(req,res){
               }
           }   
         }
+        */
     });
 });
 
