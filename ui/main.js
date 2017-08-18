3feac441c8c4d93e5efa902f67a9f53ed323d523
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-18 06:28:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,56 +1,5 @@
 console.log('Loaded!');
 
-
-//capture the resp & store 
-//render the var in span
-/*
-var button=document.getElementById('counter');
-var counter=0;
-button.onclick= function(){
-    //create a request to web server
-    var  request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-            }
-        }
-        
-    };
-    */
-    /*end of onready*/
-    
-//make a request to counter endpoint
-/*
-request.open('GET',"http://deepa042008.imad.hasura-app.io/counter",true);
-request.send(null);
-};
-var submit=document.getElementById("submit_btn");
-submit.onclick=function(){
-  //  var names=['name1','name2','name3','name4'];
-    var  request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                var names=request.responseText;
-                //converts string i.e responsetext to an object 
-                names=JSON.parse(names);
-                var list='';
-                for(var i=0;i<names.length;i++)
-                {
-                    list+='<li>'+names[i]+'</li>';
-                }
-                var ul=document.getElementById("namelist");
-                ul.innerHTML=list;
-                
-            }
-        }
-
-    };
-    */
-    
 var submit=document.getElementById("submit_btn");
 
 submit.onclick=function(){
@@ -80,6 +29,6 @@ console.log(username+":"+password);
 
 //request.open('GET','http://deepa042008.imad.hasura-app.io/submit-name?name='+name,true);
 request.open('POST','http://deepa042008.imad.hasura-app.io/login',true);
-request.setRequestHeader('Content-Type','application/json');
+request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username: username, password: password}));
 };
