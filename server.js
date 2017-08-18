9c2161d969bdd0ed9daaa4d6c65f4e934f9a66aa
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-18 07:12:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -181,8 +181,6 @@ app.post('/login',function(req,res){
     var username=req.body.username;
     var password=req.body.password;
     pool.query('SELECT * FROM "user" WHERE username = $1',[username],function(err,result){
-
-        
         if (err)
         {
             res.status(500).send(err.toString());
@@ -194,7 +192,7 @@ app.post('/login',function(req,res){
           }
           else 
           {
-            //  console.log('before dbstring'+result.rows[0].password);
+              console.log('before dbstring'+result.rows[0].password);
               var dbString=result.rows[0].password;
               var salt=dbString.spilt('$')[2];
               
