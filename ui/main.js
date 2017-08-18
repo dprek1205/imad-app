48f37eade62fc38f3ee24c8aa3855d1f760af58e
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-18 06:40:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,5 +30,7 @@ console.log(username+":"+password);
 //request.open('GET','http://deepa042008.imad.hasura-app.io/submit-name?name='+name,true);
 request.open('POST','http://deepa042008.imad.hasura-app.io/login',true);
 request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({username: username, password: password}));
+//request.send(JSON.stringify({username: username, password: password}));
+var to_send = {username: 'username', password: 'password'};
+request.send(JSON.stringify(to_send));
 };
