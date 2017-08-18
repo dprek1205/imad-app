e5657e945d36f8e70a3da069de46495dd5ccdc92
dprek1205 <30746837+dprek1205@users.noreply.github.com>
2017-08-18 07:20:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -180,6 +180,7 @@ app.post('/create-user',function(req,res) {
 app.post('/login',function(req,res){
     var username=req.body.username;
     var password=req.body.password;
+    console.log('in server post'+username+password);
     pool.query('SELECT * FROM "user" WHERE username = $1',[username],function(err,result){
         if (err)
         {
